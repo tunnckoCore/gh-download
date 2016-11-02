11355429aa413eff7c919338b0d228a271247b92
tunnckoCore <mameto_100@mail.bg>
2016-11-02 06:23:13 +0200

    chore(init): push

---
+++ test.js
@@ -0,0 +1,10 @@
+/*!
+ * gh-download <https://github.com/tunnckoCore/gh-download>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
